1ce46ed5fea4a0b5a3c12c15f897942119d67c61
ferch <ferch123@Gmail.com>
2025-03-18 22:04:57 -0600

    Actualizacion final

--- Actividad-DOM/Sistema.js
+++ Actividad-DOM/Sistema.js
@@ -2,12 +2,14 @@ const lista = document.getElementById("lista-palabra");
 const listaP = ["manzana","pera","tomate","limon","aguacate","kiwi","sandia","banana","fresa","mora","baya","pitahaya","mango","melon","cereza"];
 let intentos = 1;
 let cantaciertos = 0;
+//Determinar la palabra a adivinar.
 function ElegirPalabra(){
     let palabra = listaP[Math.floor(Math.random()*15)];
     console.log(palabra);
     return palabra;
 }
 let palabraelegida = ElegirPalabra();
+//Trasladar la palabra a la vista, guardar y cifrarla.
 function cifrar(Palabra){
     let letra = Palabra.split("");
     let x;
@@ -20,8 +22,10 @@ function cifrar(Palabra){
         lista.append(li)
     }
 }
+//se define la cantidad de letras que se debe adivinar
+let incognitas = palabraelegida.length;
 cifrar(palabraelegida);
-
+//Cada vez que se presione el boton checkeara si la letra seleccionada esta dentro de la palabra o no.
 function checkear(){
     const LetraElegida = document.getElementById("letra");
     let nuevoL;
@@ -29,16 +33,18 @@ function checkear(){
     let letra; 
     let aciertos;
     let adivino = false;
-    
+    //Elimina cualquier espacio que se llegue a ingresar en el input.
     if(LetraElegida.value.includes(" ") || LetraElegida.value == ""){
         alert("No se permiten espacios");
     }else{
+        //Evita que lo ingresado en el input tengas más de 1 caracter.
         if(LetraElegida.value.length >= 1){
             LetraElegida.value = LetraElegida.value.slice(0,1);
             letra = LetraElegida.value.toLowerCase();
         }
+        //Se divide la cadena de texto para evaluar si la letra ingresada esta dentro de la palabra y en cual posicion.
+        //Evalua si adivino la palabra o si le faltan incognitas aun.
         let palabra = palabraelegida.split("");
-        let incognitas = palabra.length;
         for(let i = 0; i < palabra.length; i++){
             if(letra === palabra[i]){
                 aciertos = document.getElementById(i);
@@ -54,6 +60,7 @@ function checkear(){
                 break;
             }
         }
+        //cada vez que presione el boton si no llega a adininar ninguna se le sumaran intentos hasta llegar a 6.
         if(!adivino){
             img = document.getElementById("img");
             intentos = intentos + 1;
@@ -69,6 +76,7 @@ function checkear(){
             } 
         } 
     }
+    //dejar vacio el input para recibir la siguiente letra.
     LetraElegida.value = null;
    
     
