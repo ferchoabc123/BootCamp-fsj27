af3b2dcc4ed36fad64894daf2009acc178002d5f
ferch <ferch123@Gmail.com>
2025-06-03 21:28:22 -0600

    Tarea-4 fix

--- PHP/Tarea4.php
+++ PHP/Tarea4.php
@@ -10,7 +10,7 @@ function burbuja($arr){
 
     for($i = 0; $i < $largo; $i++){
         for($j = 0; $j < $largo - 1;$j++){
-            if($arr[$j] > $arr[$j + 1]){
+            if($arr[$j] < $arr[$j + 1]){
                 $temp = $arr[$j];
                 $arr[$j] = $arr[$j + 1];
                 $arr[$j + 1] = $temp;
